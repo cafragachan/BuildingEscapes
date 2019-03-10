915d142cdc33a2ef2ccdd1867ec0747cf6d7a0c9
cafragachan <cafragachan@yahoo.com>
2019-03-10 14:35:01 +0000

    Layout Out Geometry
    Lecture 73

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,36 +1,37 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
-#include "GameFramework/Actor.h"
-#include "Containers/UnrealString.h"
+#include "OpenDoor.h"
+#include"GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectLocation = GetOwner()->GetActorLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s location is %s"), *ObjectName, *ObjectLocation);
-
+	// ...
+	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	
+	FRotator NewRotation = FRotator(0.1f);
+	GetOwner()->SetActorRotation(NewRotation);
+
 }
 
